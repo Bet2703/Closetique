02b4e6fa3ff26c560b432dc4e4f3c7e1ef10970b
Bet <b.dellorusso@studenti.unisa.it>
2025-07-04 10:33:57 +0200

    Modifiche alla preview dell'armadio nella homepage, modifiche a wardrobe con aggiunta del titolo e del +, creazione detail view

---
+++ Closetique/Closetique/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Closetique
+//
+//  Created by Studente on 04/07/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
